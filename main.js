e0143357ba8d3fa4944e98853ae39709c59e1d12
ammaralsaeed123 <55942208+ammaralsaeed123@users.noreply.github.com>
2019-11-10 15:25:51 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-document.getElementById(but).addEventListener ("click" ,click);
+// document.getElementById(but).addEventListener ("click" ,click);
+let dd=document.getElementById("but");
 
+dd.addEventListener("click",ev=>{
+    click()
+})
 function click(){
-    page=document.getElementById("inp").value;
-fetch('http://localhost:4000/students?page=' + page, {
+ page=document.getElementById("inp").value;
+fetch('http://localhost:4000/students/'+ page, {
             method: 'GET',
         })
         .then(res => res.json())
         .then(data => {
-            
-            for(var i=0 ; i<data.length ; i++) { 
-          document.getElementById("table").innerHTML +=' <tr><td>'+data[i].id+'</td><td>'+data[i].first_name+'</td><td>'+ data[i].last_name+'</td><td>'+data[i].grade+'</td></tr>';
-         
+          console.log(data);
+          for(let i=0;i<data.length;i++){
+          document.getElementById("table").innerHTML +=' <tr><td>'+
+          data[i].id+'</td><td>'+data[i].first_name+'</td><td>'+
+          data[i].last_name+'</td><td>'+data[i].grade+'</td></tr>';
           }
-        
-        });}
+        })
+        }
